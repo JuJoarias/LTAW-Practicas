163a23eda6a4aebc1b5f1c075e0dbb1ab7522721
juanjose <jj.arias.2020@alumnos.urjc.es>
2024-02-19 12:40:29 +0100

    tienda

--- P1/tienda.js
+++ P1/tienda.js
@@ -7,38 +7,42 @@ const PUERTO = 9090;
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        // Si la URL solicitada es la raíz, sirve el archivo HTML
-        const indexPath = path.join(__dirname, 'index.html');
-        fs.readFile(indexPath, (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error interno del servidor');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                console.log("Cargo datos")
-                res.end(data);
-            }
-        });
-    } else if (req.url === '/home/alumnos/juanjose/LTAW/LTAW-Practicas/P1/halcon_milenario.jpg') {
-        // Si la URL solicitada es la imagen, sirve la imagen
-        const imagePath = path.join(__dirname, 'halcon_milenario.jpg'); // Ruta a tu imagen
-        fs.readFile(imagePath, (err, data) => {
-            if (err) {
-                console.log("Fallo al cargar la imagen")
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error interno del servidor');
-            } else {
-                console.log("Cargo imagen")
-                res.writeHead(200, { 'Content-Type': 'image/jpeg' }); // Cambia el tipo de contenido según el formato de tu imagen
-                res.end(data);
-            }
-        });
+    const url = req.url === '/' ? 'index.html' : req.url;
+    const filePath = path.join(__dirname, url);
+    const extension = path.extname(filePath);
+    let contentType = 'text/html';
+    //-- /workspaces/LTAW-Practicas/P1/ -- Si trabajo desde casa este es el path si tengo nodejs en este portatil
+    //-- /home/alumnos/juanjose/LTAW/LTAW-Practicas/P1/ -- si trabajo desde la uni este es el path
+  //-- Indicamos que se ha recibido una petición probando
+  console.log("Petición recibida!");
+
+  switch (extension) {
+    case '.html':
+        contentType = 'text/html';
+        break;
+    case '.jpg':
+        console.log('cambio a imagen');
+        contentType = 'image/jpg';
+        break;
+  }
+
+  fs.readFile(filePath, (err, Content) => {
+    if (err) {
+        if (err.code == 'ENOENT') {
+            fs.readFile(path.join(__dirname, '404.html'), (err, Content) => {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end(Content, 'utf-8');
+            });
+        } else {
+            res.writeHead(500);
+            console.log("Fallo en el servidor!");
+            res.end('Error interno del servidor');  
+        }
     } else {
-        // Si la URL solicitada no coincide con ninguna ruta conocida, responde con un 404
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Archivo no encontrado');
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(Content, 'utf-8');
     }
+    });
 }); 
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
