365671f2cd06ff0b3e627388ce4d4b831d90a467
juanjose <jj.arias.2020@alumnos.urjc.es>
2024-02-19 11:57:58 +0100

    imagen mas prueba

--- P1/tienda.js
+++ P1/tienda.js
@@ -7,22 +7,36 @@ const PUERTO = 9090;
 
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
-    const filePath = path.join("/workspaces/LTAW-Practicas/P1/", 'index.html');
-    //-- /workspaces/LTAW-Practicas/P1/ -- Si trabajo desde casa este es el path si tengo nodejs en este portatil
-    //-- /home/alumnos/juanjose/LTAW/LTAW-Practicas/P1/ -- si trabajo desde la uni este es el path
-  //-- Indicamos que se ha recibido una petición 
-  console.log("Petición recibida!");
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        console.log("Fallo en el servidor!");
-        res.end('Error interno del servidor');
+    if (req.url === '/') {
+        // Si la URL solicitada es la raíz, sirve el archivo HTML
+        const indexPath = path.join(__dirname, 'index.html');
+        fs.readFile(indexPath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                console.log("Cargo datos")
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/home/alumnos/juanjose/LTAW/LTAW-Practicas/P1/halcon_milenario.jpg') {
+        // Si la URL solicitada es la imagen, sirve la imagen
+        const imagePath = path.join(__dirname, 'imagen.jpg'); // Ruta a tu imagen
+        fs.readFile(imagePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error interno del servidor');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'image/jpeg' }); // Cambia el tipo de contenido según el formato de tu imagen
+                res.end(data);
+            }
+        });
     } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
+        // Si la URL solicitada no coincide con ninguna ruta conocida, responde con un 404
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Archivo no encontrado');
     }
-    });
 }); 
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
