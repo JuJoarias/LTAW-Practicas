e647a436b26ddc40f13302935b568555b99d0cf7
juanjose <jj.arias.2020@alumnos.urjc.es>
2024-04-08 12:11:39 +0200

    servidor funciona, peta el cliente

--- P2/tienda.js
+++ P2/tienda.js
@@ -5,7 +5,7 @@ tienda_json = fs.readFileSync('P2/tienda.json','utf-8')
 const usuarios = JSON.parse(tienda_json).usuarios;
 const productos = JSON.parse(tienda_json).productos;
 indice = fs.readFileSync("P2/index.html", "utf-8")
-console.log(usuarios[0].usuario)
+//console.log(usuarios[0].usuario)
 tienda = JSON.parse(tienda_json)
 //console.log("Usuario en la tienda: " + tienda.usuarios[0].usuario);
 
@@ -90,6 +90,9 @@ const server = http.createServer((req, res) => {
     const url = req.url === '/' ? 'index.html' : req.url;
     const filePath = path.join(__dirname, url);
     const extension = path.extname(filePath);
+    const myURL = new URL(req.url, 'http://' + req.headers['host']);  
+    let recurso = myURL.pathname;
+    recurso = recurso.slice(1);
     let contentType = 'text/html';
     let user = get_user(req);
     
@@ -111,6 +114,62 @@ const server = http.createServer((req, res) => {
         contentType = 'audio/mpeg';
         break;
     };
+
+    switch (recurso) {
+      case '':
+            console.log("Main page");
+            break;
+      case 'productos':
+            console.log("Peticion de Productos!")
+            contentType = "application/json";
+
+            //-- Leer los parámetros
+            let param1 = myURL.searchParams.get('param1');
+
+            param1 = param1.toUpperCase();
+
+            console.log("  Param: " +  param1);
+
+            let result = [];
+            
+            for (let prod of productos) {
+                
+                //prod = ShowDescription().split(",")[i]
+                console.log('Prod: '+prod.nombre)
+                //-- Pasar a mayúsculas
+                prodU = prod.nombre.toUpperCase();
+                
+                //-- Si el producto comienza por lo indicado en el parametro
+                //-- meter este producto en el array de resultados
+                if (prodU.startsWith(param1)) {
+                    result.push(prod.nombre);
+                }
+                
+            }
+            console.log(result);
+            content = JSON.stringify(result);
+            break;
+
+        case 'cliente.js':
+            //-- Leer fichero javascript
+            console.log("recurso: " + recurso);
+            recurso = `P2/` + recurso;
+            fs.readFile(recurso, 'utf-8', (err,data) => {
+                if (err) {
+                    console.log("Error: " + err)
+                    return;
+                } else {
+                  console.log('Todo ok')
+                  res.setHeader('Content-Type', 'application/javascript');
+                  res.write(data);
+                  res.end();
+                }
+            });
+            
+            return;
+            
+    }
+
     if (req.url == '/log_in' ) {
         
         Content = indice;
