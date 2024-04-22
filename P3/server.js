46b29caf886969233465c950dd1baac5aa37adf0
juanjose <jj.arias.2020@alumnos.urjc.es>
2024-04-22 12:15:31 +0200

    avances con el username

--- P3/server.js
+++ P3/server.js
@@ -3,6 +3,7 @@ const socket = require('socket.io');
 const http = require('http');
 const express = require('express');
 const colors = require('colors');
+const cookieParser = require('cookie-parser');
 
 const PUERTO = 9090;
 let n_clientes = 0;
@@ -10,6 +11,7 @@ let comandos = ['/help', '/list', '/hello', '/date']
 
 //-- Crear una nueva aplciacion web
 const app = express();
+app.use(cookieParser());
 
 //-- Crear un servidor, asosiaco a la App de express
 const server = http.Server(app);
@@ -27,20 +29,36 @@ app.use(express.static('public'));
 //------------------- GESTION SOCKETS IO
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
+
   n_clientes += 1
   console.log('** NUEVA CONEXIÓN **'.yellow);
-  io.send('Usuario nuevo conectado'); 
+  io.emit('chatMessage', `Server: Usuario nuevo conectado`); 
+
+  // Tomamos la cookie del username
+  socket.on('username', (username) => {
+    // Establecer el nombre de usuario como cookie
+    socket.handshake.headers.cookie = `username=${username}`;
+    
+    // Emitir un evento para confirmar al cliente que se ha establecido el nombre de usuario
+    socket.emit('usernameSet', username);
+});
+
   //-- Evento de desconexión
   socket.on('disconnect', function(){
     console.log('** CONEXIÓN TERMINADA **'.yellow);
     n_clientes -=1
-    io.send('Usuario desconectado'); 
+    io.emit('chatMessage', `Server: Usuario desconectado`); 
   });  
 
   //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
-  socket.on("message", (msg)=> {
-    console.log("Mensaje Recibido!: " + msg.blue);
+  socket.on("chatMessage", (msg)=> {
+    console.log("Mensaje Recibido: " + msg);
 
+    const username = socket.handshake.headers.cookie
+            ? socket.handshake.headers.cookie.split('=')[1]
+            : 'Anonymous';
+
+    console.log('El username es: ' + username)
     // compruebo si es un comando
     if (comandos.includes(msg)){
       console.log('Es un comando')
@@ -64,12 +82,12 @@ io.on('connect', (socket) => {
         mensaje = `Hoy es: ${day}-${month}-${year}`
       }
       // mando el resultado solo al cliente que lo mando 
-      socket.send(mensaje);
+      socket.emit('chatMessage', `Server: ${mensaje}`);
       return;
     }
 // para lo del user mirar con socket.emit
     //-- Reenviarlo a todos los clientes conectados si no es un comando
-    io.send(msg); 
+    io.emit('chatMessage', `${username}: ${msg}`);
   });
 
 });
